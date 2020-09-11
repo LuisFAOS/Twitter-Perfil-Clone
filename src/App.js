c984743eb3993b9ab5e8ddcfb190de6a3d68ef29
Luis Felipe <luisFAOSetecH@gmail.com>
2020-09-11 20:12:26 -0300

    adding containers, styles and settings files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyles.js'
+import Layout from './components/Layout'
+function App() {
+  return (
+    <>
+      <GlobalStyles/>
+      <Layout/>
+    </>
+  );
+}
+
+export default App;
